9c792341b9e6e71534c2551f7a6d8971535a323f
Ivan Vishnevskiy <io@forgetable.ru>
2024-08-28 19:46:22 +0400

    fix indentation

--- frontend/src/components/Menu/button.tsx
+++ frontend/src/components/Menu/button.tsx
@@ -4,20 +4,20 @@ import styles from 'src/styles/components/menu/button.module.scss';
 import Image from 'next/image';
 
 interface IMenuButtonProps {
-    text: string;
-    path: string;
-    iconPath: string;
-    isSelected: boolean;
+  text: string;
+  path: string;
+  iconPath: string;
+  isSelected: boolean;
 }
 
 const MenuButton: React.FC<IMenuButtonProps> = (props: IMenuButtonProps) => {
-    const { iconPath } = props;
-    return (
-        <Link href={props.path} className={styles.button + ` ${props.isSelected ? styles.selected : ''}`}>
-            <Image src={iconPath} alt={props.text} width="29" height="29" className={styles.icon} />
-            <p>{props.text}</p>
-        </Link>
-    );
+  const { iconPath } = props;
+  return (
+    <Link href={props.path} className={styles.button + ` ${props.isSelected ? styles.selected : ''}`}>
+    <Image src={iconPath} alt={props.text} width="29" height="29" className={styles.icon} />
+    <p>{props.text}</p>
+    </Link>
+  );
 };
 
 export default MenuButton;
