4c7a37dea5385b6d4a9641211c421db62339c297
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-03 18:48:19 +0400

    Coin and monospace font for tap counter

--- frontend/src/components/TapCounter/TapCounter.tsx
+++ frontend/src/components/TapCounter/TapCounter.tsx
@@ -1,26 +1,7 @@
 import React from "react";
 import styles from "src/styles/components/tapCounter/tapCounter.module.scss";
 import { useTapCounter } from "src/shared/context/TapCounterContext";
-
-/**
- * Calculates the font size for the tap counter, given the tap count.
- * The font size is linearly interpolated between 60px for 6 digits
- * and 35px for 18 digits.
- * @returns the font size as a string, e.g. "60px"
- */
-const calculateFontSize = (tapCount: number) => {
-  const digits = tapCount.toString().length;
-  if (digits <= 6) {
-    return "60px";
-  }
-  const maxFontSize = 60;
-  const minFontSize = 35;
-  const maxDigits = 18;
-  const fontSize =
-    ((maxFontSize - minFontSize) * (maxDigits - digits)) / (maxDigits - 6) +
-    minFontSize;
-  return `${fontSize}px`;
-};
+import Cost from "../shared/Cost";
 
 const TapCounter: React.FC = () => {
   const { data } = useTapCounter();
@@ -29,8 +10,10 @@ const TapCounter: React.FC = () => {
   const formattedTapCount = numberFormatter.format(data.tapCount);
   return (
     <div className={styles.tapCounter}>
-      <h1 style={{ fontSize: calculateFontSize(data.tapCount) }}>
-        {formattedTapCount}
+      <h1 style={{ fontSize: 30, fontFamily: "MartianMono" }}>
+        <Cost size={32}>
+          <span>{formattedTapCount}</span>
+        </Cost>
       </h1>
     </div>
   );
