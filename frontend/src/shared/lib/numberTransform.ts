bd1025b9d41a92c276120479d407862b482c4317
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-03 15:09:35 +0400

    Prettier formatting

--- frontend/src/shared/lib/numberTransform.ts
+++ frontend/src/shared/lib/numberTransform.ts
@@ -1,14 +1,14 @@
-  /**
-   * Takes a number and returns a string that is either the number itself
-   * or the number formatted with one of the following suffixes:
-   * - 'B' for billions
-   * - 'M' for millions
-   * - 'K' for thousands
-   * The suffix is chosen based on the number. If the number is less than 1000,
-   * the number itself is returned.
-   * @param {number} num - The number to format
-   * @returns {string} The formatted string
-   */
+/**
+ * Takes a number and returns a string that is either the number itself
+ * or the number formatted with one of the following suffixes:
+ * - 'B' for billions
+ * - 'M' for millions
+ * - 'K' for thousands
+ * The suffix is chosen based on the number. If the number is less than 1000,
+ * the number itself is returned.
+ * @param {number} num - The number to format
+ * @returns {string} The formatted string
+ */
 const numberTransform = (num: number): string => {
   if (num < 1000) return num.toString();
 
@@ -16,10 +16,10 @@ const numberTransform = (num: number): string => {
   if (billions >= 1) return `${Math.floor(billions)}B`;
 
   const millions = num / 1000000;
-  if (millions >= 1) return `${(millions).toFixed(2).replace(/\.00$/, '')}M`;
+  if (millions >= 1) return `${millions.toFixed(2).replace(/\.00$/, "")}M`;
 
   const thousands = num / 1000;
-  return `${(thousands).toFixed(2).replace(/\.00$/, '')}K`;
+  return `${thousands.toFixed(2).replace(/\.00$/, "")}K`;
 };
 
 export default numberTransform;
