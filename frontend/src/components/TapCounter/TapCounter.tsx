bc7330ae00c4f3744059130fcf522d630256cb97
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-03 19:37:07 +0400

    Add passive income info to tap counter

--- frontend/src/components/TapCounter/TapCounter.tsx
+++ frontend/src/components/TapCounter/TapCounter.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from "src/styles/components/tapCounter/tapCounter.module.scss";
 import { useTapCounter } from "src/shared/context/TapCounterContext";
 import Cost from "../shared/Cost";
+import numberTransform from "src/shared/lib/numberTransform";
 
 const TapCounter: React.FC = () => {
   const { data } = useTapCounter();
 
   const numberFormatter = new Intl.NumberFormat("en-US");
-  const formattedTapCount = numberFormatter.format(data.tapCount);
+  const formattedTapCount = useMemo(() => numberFormatter.format(data.tapCount), [data.tapCount]);
+  const formattedPassiveIncome = useMemo(() => numberTransform(data.passiveIncome * 3600), [data.passiveIncome]);
   return (
     <div className={styles.tapCounter}>
       <h1 style={{ fontSize: 30, fontFamily: "MartianMono" }}>
@@ -15,6 +17,9 @@ const TapCounter: React.FC = () => {
           <span>{formattedTapCount}</span>
         </Cost>
       </h1>
+      <p>
+        {formattedPassiveIncome} per hour
+      </p>
     </div>
   );
 };
