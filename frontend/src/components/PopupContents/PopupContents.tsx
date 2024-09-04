880a8a76f1170d9db80cfbd42723680a7d87d08c
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-04 21:01:46 +0400

    Popup contents component

---
+++ frontend/src/components/PopupContents/PopupContents.tsx
@@ -0,0 +1,24 @@
+import styles from "src/styles/components/popupContents/popupContents.module.scss";
+
+interface PopupContentsProps {
+  children?: JSX.Element;
+  direction?: "row" | "column";
+  className?: string;
+}
+
+const PopupContents: React.FC<PopupContentsProps> = ({
+  children,
+  direction = "column",
+  className,
+}) => {
+  return (
+    <div
+      className={styles.popupContents + ' ' + className}
+      style={{ flexDirection: direction }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default PopupContents;
