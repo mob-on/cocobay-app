2e1263efd7dd557998b198578618de9ee58c4a28
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-16 17:32:42 +0400

    shared, dashboard and import updates

---
+++ packages/dashboard/next.config.mjs
@@ -0,0 +1,44 @@
+import withPlugins from "next-compose-plugins";
+import withImages from "next-images";
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "export",
+  reactStrictMode: true,
+  transpilePackages: ["antd"],
+  trailingSlash: true,
+  images: {
+    disableStaticImages: true,
+    unoptimized: true, //required by next output:export
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["pino", "pino-pretty"],
+  },
+  // webpack(config) {
+  //   config.module.rules.push(
+  //     {
+  //       test: /\.svg$/,
+  //       use: [
+  //         {
+  //           loader: '@svgr/webpack',
+  //           options: {
+
+  //           },
+  //         },
+  //       ]
+  //     }
+  //   );
+  //   return config;
+  // },
+};
+
+const defaultConfig = {};
+
+const nextConfigWithPlugins = async (phase) =>
+  withPlugins([[withImages()]], nextConfig)(phase, {
+    defaultConfig,
+  });
+
+export default nextConfigWithPlugins;
