5bd2819b2aac3618ddb662f5aa587b100ced49f5
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-17 15:26:47 +0400

    Updated tracking

--- packages/frontend/src/shared/hooks/useTracking.ts
+++ packages/frontend/src/shared/hooks/useTracking.ts
@@ -1,11 +1,14 @@
 import toISOString from "@lib/toISOString";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import { useMainApiConfig } from "../api/main/config";
 import { useTrackingApi } from "../api/useTrackingApi";
 import useLogger from "../hooks/useLogger";
 
-const MAX_EVENT_BUFFER_SIZE = 3;
+const MAX_EVENT_BUFFER_SIZE = 5;
+const REPORT_TIMEOUT = 2000;
+const ERROR_TIMEOUT = 5000;
 
 export enum TrackerEventType {
   PAGE_VIEW = "page_view",
@@ -23,6 +26,7 @@ export interface ITrackerEvent<T = any> {
   url: string;
   title: string;
   ts: string;
+  id: string;
 }
 
 export type IPageViewEvent = ITrackerEvent<{
@@ -34,6 +38,37 @@ export type ITestEvent = ITrackerEvent<{
   testData: any;
 }>;
 
+const useCreateEvent = (): ((
+  event: TrackerEventType,
+  data: any,
+  tags: string[],
+) => ITrackerEvent | null) => {
+  const logger = useLogger("useCreateEvent");
+  return (event: TrackerEventType, data: any = {}, tags: string[] = []) => {
+    let bufferEvent: ITrackerEvent | null = null;
+    switch (event) {
+      case TrackerEventType.PAGE_VIEW:
+      case TrackerEventType.CLICK_LINK:
+        bufferEvent = { event, data: {} } as IPageViewEvent;
+        break;
+      case TrackerEventType.TEST:
+        bufferEvent = { event, data } as ITestEvent;
+        break;
+      default:
+        logger.error(`Event type not implemented: ${event}`);
+        return null;
+    }
+
+    bufferEvent.tags = tags;
+    bufferEvent.url = window.location.pathname;
+    bufferEvent.title = document.title;
+    bufferEvent.ts = toISOString(new Date());
+    bufferEvent.id = uuidv4();
+
+    return bufferEvent;
+  };
+};
+
 /**
  * Hook to track events in the app.
  *
@@ -41,7 +76,9 @@ export type ITestEvent = ITrackerEvent<{
  * and the second function can be used to report all events in the buffer.
  *
  * If the buffer of events reaches {MAX_EVENT_BUFFER_SIZE} or
- * if the last send was more than 1000ms ago, then the buffer is sent to the server.
+ * if the last send was more than {REPORT_TIMEOUT}ms ago, then the buffer is sent to the server.
+ *
+ * If report fails, the buffer is returned to the buffer and we try to send it again in {ERROR_TIMEOUT}ms
  *
  * If the user navigates away from the page or closes the tab, the buffer is sent to the server.
  *
@@ -54,15 +91,16 @@ export const useTracking = (): [
   () => void,
 ] => {
   const logger = useLogger("useTracker");
+  const createEvent = useCreateEvent();
   const [axios] = useMainApiConfig();
   const buffer = useRef<ITrackerEvent[]>([]);
   const lastSendTimestamp = useRef<number>(Date.now());
   const timeoutController = useRef<number | undefined>(undefined);
+  const errorTimeoutController = useRef<number | undefined>(undefined);
 
   const trackingApi = useTrackingApi();
 
   const handleNavigationOrTabClose = useCallback(() => {
-    console.log(102, "handleNavigationOrTabClose");
     const bufferCopy = [...buffer.current];
     buffer.current = [];
 
@@ -74,26 +112,23 @@ export const useTracking = (): [
   }, [buffer, trackingApi]);
 
   const shouldSend = useCallback(() => {
-    const timestamp = Number(new Date());
-
-    if (
+    const timestamp = Date.now();
+    return (
       buffer.current.length >= MAX_EVENT_BUFFER_SIZE ||
-      timestamp - lastSendTimestamp.current >= 1000
-    )
-      return true;
-    else return false;
-  }, [lastSendTimestamp]);
+      timestamp - lastSendTimestamp.current >= REPORT_TIMEOUT
+    );
+  }, []);
 
   const report = useCallback(
     (force = false) => {
       const shouldSendValue = force ? true : shouldSend();
       const bufferCopy = [...buffer.current];
-
+      clearTimeout(timeoutController.current);
+      clearTimeout(errorTimeoutController.current);
       if (!shouldSendValue && bufferCopy.length) {
-        clearTimeout(timeoutController.current);
         timeoutController.current = window.setTimeout(
           () => report(),
-          lastSendTimestamp.current - Number(new Date()) + 1000,
+          lastSendTimestamp.current - Number(new Date()) + REPORT_TIMEOUT,
         );
 
         return;
@@ -103,12 +138,23 @@ export const useTracking = (): [
 
       lastSendTimestamp.current = Number(new Date());
 
-      buffer.current = [];
       trackingApi.mutate(bufferCopy, {
         onError: (e: Error) => {
           logger.error(e);
-          buffer.current = [...bufferCopy, ...buffer.current]; // Return data to buffer
-          window.setTimeout(report, 1000);
+          errorTimeoutController.current = window.setTimeout(
+            report,
+            ERROR_TIMEOUT,
+          );
+        },
+        onSuccess: () => {
+          const succeededIds = new Set();
+          for (let i = 0; i < bufferCopy.length; i++) {
+            succeededIds.add(bufferCopy[i].id);
+          }
+
+          buffer.current = buffer.current.filter(
+            (event) => !succeededIds.has(event.id),
+          );
         },
       });
     },
@@ -116,33 +162,26 @@ export const useTracking = (): [
   );
 
   useEffect(() => {
-    window.addEventListener("beforeunload", handleNavigationOrTabClose);
+    document.addEventListener("visibilitychange", handleNavigationOrTabClose);
     return () =>
-      window.removeEventListener("beforeunload", handleNavigationOrTabClose);
+      document.removeEventListener(
+        "visibilitychange",
+        handleNavigationOrTabClose,
+      );
   }, [handleNavigationOrTabClose]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutController.current) {
+        clearTimeout(timeoutController.current);
+      }
+    };
+  }, []);
 
   return [
     (event: TrackerEventType, data: any = {}, ...tags) => {
-      let bufferEvent: ITrackerEvent | null = null;
-      switch (event) {
-        case TrackerEventType.PAGE_VIEW:
-        case TrackerEventType.CLICK_LINK:
-          bufferEvent = { event, data: {} } as IPageViewEvent;
-          break;
-        case TrackerEventType.TEST:
-          bufferEvent = { event, data } as ITestEvent;
-          break;
-      }
-
-      if (!bufferEvent) {
-        logger.error(`Event type not implemented: ${event}`);
-        return;
-      }
-
-      bufferEvent.tags = tags;
-      bufferEvent.url = window.location.pathname;
-      bufferEvent.title = document.title;
-      bufferEvent.ts = toISOString(new Date());
+      const bufferEvent = createEvent(event, data, tags);
+      if (!bufferEvent) return;
 
       buffer.current = [...buffer.current, bufferEvent];
       if (event === TrackerEventType.CLICK_LINK) handleNavigationOrTabClose();
