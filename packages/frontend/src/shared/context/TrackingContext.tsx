5a570c25711544ba1edc8cba4976ed0ac77c134c
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-17 14:49:27 +0400

    Updates to TrackingContext

--- packages/frontend/src/shared/context/TrackingContext.tsx
+++ packages/frontend/src/shared/context/TrackingContext.tsx
@@ -1,11 +1,11 @@
 import useLogger from "@hooks/useLogger";
-import { TrackerEventTypes, useTracking } from "@hooks/useTracking";
+import { TrackerEventType, useTracking } from "@hooks/useTracking";
 import { usePathname } from "next/navigation";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useCallback, useContext, useEffect } from "react";
 
-export const TrackingContext = createContext<{ track: (any) => void } | null>(
-  null,
-);
+export const TrackingContext = createContext<{
+  track: (event: TrackerEventType, data: any, ...tags: string[]) => void;
+} | null>(null);
 
 export const useTrackerContext = () => {
   const logger = useLogger("useTracker");
@@ -22,10 +22,59 @@ export const TrackingProvider: React.FC<{ children: React.JSX.Element }> = ({
 }) => {
   const pathname = usePathname();
   const [track] = useTracking();
-
   useEffect(() => {
-    track(TrackerEventTypes.PAGE_VIEW);
+    track(TrackerEventType.PAGE_VIEW);
   }, [pathname]);
+
+  // NOTE: Use this commented code to test tracking.
+  // TODO: Remove it, after we're done with this!
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     track(
+  //       TrackerEventType.TEST,
+  //       {
+  //         testData: {
+  //           test: "test",
+  //           test2: [1, "test2", { test3: "test3" }],
+  //         },
+  //       },
+  //       "test-tag",
+  //     );
+  //   }, 200);
+  //   setTimeout(() => {
+  //     track(
+  //       TrackerEventType.TEST,
+  //       {
+  //         testData: {
+  //           test: "test1",
+  //         },
+  //       },
+  //       "test-tag1",
+  //     );
+  //   }, 1300);
+  //   setTimeout(() => {
+  //     track(
+  //       TrackerEventType.TEST,
+  //       {
+  //         testData: {
+  //           test: "test2",
+  //         },
+  //       },
+  //       "test-tag2",
+  //     );
+  //   }, 2100);
+  //   setTimeout(() => {
+  //     track(
+  //       TrackerEventType.TEST,
+  //       {
+  //         testData: {
+  //           test: "test3",
+  //         },
+  //       },
+  //       "test-tag3",
+  //     );
+  //   }, 2120);
+  // }, []);
 
   return (
     <TrackingContext.Provider value={{ track }}>
