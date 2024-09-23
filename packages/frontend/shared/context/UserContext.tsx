384fcfc8e58e5f01fd6c109a983fd6dd0fa1ea0d
Ivan Vishnevskiy <io@forgetable.ru>
2024-09-23 15:39:09 +0400

    Optimize user context

--- packages/frontend/shared/context/UserContext.tsx
+++ packages/frontend/shared/context/UserContext.tsx
@@ -1,20 +1,18 @@
 "use client";
 
+import { User } from "@shared/src/interfaces";
 import { createContext, useContext, useEffect, useState } from "react";
 
 import useTelegram from "../hooks/useTelegram";
-import { IUser } from "../services/useUserService";
 import { ILoadingContextResource, useLoading } from "./LoadingContext";
 
 export interface IUserContext {
-  data: IUser;
-  setData: (data: IUser | ((data: IUser) => IUser)) => void;
+  data: User;
+  setData: (data: User | ((data: User) => User)) => void;
 }
 
-const defaultUserData = {};
-
 const UserContext = createContext({
-  data: defaultUserData as IUser,
+  data: {} as User,
   setData: () => {},
 } as IUserContext);
 
@@ -27,20 +25,22 @@ export const UserContextProvider = ({
   children: React.JSX.Element;
 }) => {
   const [WebApp] = useTelegram();
-  const [data, setData] = useState<IUser>(defaultUserData as IUser);
   const loading = useLoading();
-  const user: ILoadingContextResource<IUser> = loading.resources["user"];
+  const user: ILoadingContextResource<User> = loading.resources["user"];
+
+  const [data, setData] = useState<User>(user?.data ?? ({} as User));
 
   useEffect(() => {
     if (user?.data) {
       setData((oldData) => ({ ...oldData, ...user.data }));
     }
-    // fetch data and put it into the state.
   }, [user?.data]);
 
   useEffect(() => {
     if (WebApp) {
-      setData((oldData) => ({ ...oldData, ...WebApp.initDataUnsafe.user }));
+      setData(
+        (oldData) => ({ ...oldData, ...WebApp.initDataUnsafe.user }) as User,
+      );
     }
   }, [WebApp]);
 
