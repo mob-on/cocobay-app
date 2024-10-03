c1c0a72eff464c660e5b717b2c988c378fc63a76
Ivan Vishnevskiy <io@forgetable.ru>
2024-10-03 21:46:51 +0400

    Revert resources context to pre-suspense version

--- packages/frontend/shared/context/ResourcesContext.tsx
+++ packages/frontend/shared/context/ResourcesContext.tsx
@@ -1,41 +1,39 @@
 "use client";
+import { TelegramWebappAuthDto } from "@shared/src/dto/auth/telegram-webapp-auth.dto";
 import { GameDataDto } from "@shared/src/dto/gameData.dto";
 import { UserDto } from "@shared/src/dto/user.dto";
-import { LoadingScreen } from "@src/components/LoadingScreen";
-import {
-  createContext,
-  ReactNode,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { validate } from "class-validator";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { GAME_DATA_QUERY_KEY, useGameDataApi } from "../api/useGameDataApi";
+import { useUserApi } from "../api/useUserApi";
 import useLogger from "../hooks/useLogger";
-import useUserService, { LOGIN_QUERY_KEY } from "../services/useUserService";
+import useTelegram from "../hooks/useTelegram";
+import { Feature } from "../lib/FeatureFlags";
+import { USER_QUERY_KEY } from "../services/useUserService";
 import { useErrorContext } from "./ErrorContext";
-import { useStorage, useStoredApiUrl } from "./LocalStorageContext";
+import { useStoredField } from "./LocalStorageContext";
 
-type ResourceStatus = "pending" | "loaded" | "errored";
+const MAX_TRIES = 3;
 
-export interface Resource<T> {
-  name: string;
-  status: ResourceStatus;
-  data: T | null;
-  promise: Promise<T>;
-  error?: Error;
+type IResourcesContextResourceStatus = "pending" | "loaded" | "errored";
+
+export interface IResourcesContextResource<T> {
+  status: IResourcesContextResourceStatus;
+  data?: T;
 }
 
-interface ResourceContext {
+export interface IResourcesContext {
   allLoaded: boolean;
-  updateResourceStatus: <T>(
+  updateResourceStatus: (
     resourceName: string,
-    status: ResourceStatus,
-    data?: T,
+    status: IResourcesContextResourceStatus,
   ) => void;
-  resources: Record<string, Resource<any>>;
+  resources: IResourcesContextResources;
+}
+
+export interface IResourcesContextResources {
+  [key: string]: IResourcesContextResource<any>;
 }
 
 interface ResourceToLoad<T> {
@@ -43,169 +41,142 @@ interface ResourceToLoad<T> {
   fn: () => Promise<T>;
 }
 
-const cache: Record<
-  string,
-  { data: any; promise: Promise<void>; status: ResourceStatus; error?: Error }
-> = {};
+const ResourcesContext = createContext({} as IResourcesContext);
 
-const ResourcesContext = createContext({} as ResourceContext);
+export const ResourcesProvider = ({ children }) => {
+  const errorContext = useErrorContext();
+  const [mainApiBaseUrl] = useStoredField("API_BASE_URL");
+  const [isDataRequested, setIsDataRequested] = useState(false);
+  const [resources, setResources] = useState<IResourcesContextResources>({}); // { resource1: 'pending', resource2: 'loaded', ... }
+  const logger = useLogger("ResourcesProvider");
+  const [WebApp] = useTelegram();
+  const userApi = useUserApi();
+  const gameDataApi = useGameDataApi();
 
-/**
- * Returns an array of Resource objects for the given resourcesToLoad.
- * If a resource has already been loaded, it will be returned immediately.
- * If a resource has not yet been loaded, a promise for the resource will be
- * added to the cache and returned.
- * If any of the resources are still pending (i.e. not loaded or errored), this
- * function will throw a promise that will resolve when all resources have
- * finished loading.
- */
-const useMultipleResources = <T extends any[]>(
-  resources: ResourceToLoad<T[number]>[],
-): Resource<T[number]>[] => {
-  if (typeof window === "undefined") return [];
-
-  const cachedResources = resources.map((resource) => {
-    if (cache[resource.name]) {
-      return {
-        name: resource.name,
-        status: cache[resource.name].status,
-        data: cache[resource.name].data as T[number],
-        promise: cache[resource.name].promise as Promise<T[number]>,
-      };
+  const login = async (tries = 0) => {
+    if (tries >= MAX_TRIES) {
+      errorContext.showErrorScreen({
+        message: "Wasn't able to log in/register",
+      });
+      throw new Error("Too many tries");
     }
 
-    const promise = resource
-      .fn()
-      .then((data: T[number]) => {
-        cache[resource.name] = { data, promise, status: "loaded" };
-      })
-      .catch((error) => {
-        cache[resource.name] = {
-          data: null,
-          promise,
-          status: "errored",
-          error,
-        };
-      });
+    let initDataRaw = WebApp?.initData;
+    if (!initDataRaw) {
+      if (Feature.DEV_MODE) {
+        logger.warn(
+          "No user information available, defaulting to user ID 1 (dev mode) and dummy bot token",
+        );
 
-    cache[resource.name] = { data: null, promise, status: "pending" };
+        initDataRaw =
+          "auth_date=1&can_send_after=10000&chat=%7B%22id%22%3A1%2C%22type%22%3A%22group%22%2C%22title%22%3A%22chat-title%22%2C%22photo_url%22%3A%22group%22%2C%22username%22%3A%22my-chat%22%7D&chat_instance=888&chat_type=sender&query_id=QUERY&receiver=%7B%22added_to_attachment_menu%22%3Afalse%2C%22allows_write_to_pm%22%3Atrue%2C%22first_name%22%3A%22receiver-first-name%22%2C%22id%22%3A991%2C%22is_bot%22%3Afalse%2C%22is_premium%22%3Atrue%2C%22language_code%22%3A%22ru%22%2C%22last_name%22%3A%22receiver-last-name%22%2C%22photo_url%22%3A%22receiver-photo%22%2C%22username%22%3A%22receiver-username%22%7D&start_param=debug&user=%7B%22added_to_attachment_menu%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22first_name%22%3A%22John%22%2C%22id%22%3A1%2C%22is_bot%22%3Afalse%2C%22is_premium%22%3Afalse%2C%22language_code%22%3A%22en%22%2C%22last_name%22%3A%22Doe%22%2C%22photo_url%22%3A%22user-photo%22%2C%22username%22%3A%22user-username%22%7D&hash=632c77eeb73df914625433cb07a7939e8f688524ad3957dfc91a0a1af5b7c983";
+      } else {
+        logger.error("No user information available");
+        errorContext.showErrorScreen({
+          message: "No user information available",
+        });
+        throw new Error("No user information available");
+      }
+    }
 
-    return {
-      name: resource.name,
-      status: "pending" as ResourceStatus,
-      data: null as T[number] | null,
-      promise: promise as Promise<T[number]>,
-    };
-  });
+    try {
+      const loginDto = await userApi.login(
+        new TelegramWebappAuthDto({
+          initDataRaw,
+        }),
+      );
+      const userDto = new UserDto(loginDto.user);
+      const userDtoErrors = await validate(userDto);
+      if (userDtoErrors.length) {
+        logger.error("Failed to validate user dto", userDtoErrors);
+        throw new Error("Failed to validate user dto");
+      }
+      return loginDto.user;
+    } catch (err) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return login(tries + 1);
+    }
+  };
 
-  const pendingPromises = cachedResources
-    .filter((resource) => resource.status === "pending")
-    .map((resource) => resource.promise);
+  const updateResourceStatus = (
+    resourceName: string,
+    status: IResourcesContextResourceStatus,
+    data?: any,
+  ) => {
+    setResources((prev) => {
+      const current = prev[resourceName];
+      if (current?.status === status && current?.data === data) {
+        return prev; // No change, avoid state update
+      }
+      return {
+        ...prev,
+        [resourceName]: {
+          ...(prev[resourceName] || {}),
+          status,
+          data,
+        },
+      };
+    });
+  };
 
-  if (pendingPromises.length > 0) throw Promise.all(pendingPromises);
+  const initializeResources = async (resourceList: ResourceToLoad<any>[]) => {
+    const initialResources = resourceList.reduce((acc, resource) => {
+      acc[resource.name] = {
+        status: "pending",
+        data: null,
+      };
+      return acc;
+    }, {} as IResourcesContextResources);
 
-  return cachedResources;
-};
+    setResources(initialResources);
 
-export const ResourcesProvider = ({ children }: { children: ReactNode }) => {
-  const mainApiBaseUrl = useStoredApiUrl();
-  const [isDataRequested, setIsDataRequested] = useState(false);
-  const [resources, setResources] = useState<Record<string, Resource<unknown>>>(
-    {} as Record<string, Resource<unknown>>,
-  );
-  const logger = useLogger("ResourcesProvider");
-  const errorContext = useErrorContext();
-  const gameDataApi = useGameDataApi();
-  const { login } = useUserService();
+    const updatedResources = { ...initialResources };
 
-  const updateResourceStatus = useCallback(
-    <T,>(resourceName: string, status: ResourceStatus, data?: T) => {
-      setResources((prev) => {
-        if (
-          prev[resourceName]?.status === status &&
-          prev[resourceName]?.data === data
-        )
-          return prev;
-        return {
-          ...prev,
-          [resourceName]: { ...(prev[resourceName] || {}), status, data },
-        };
-      });
-    },
-    [],
-  );
+    // Load all resources concurrently
+    await Promise.all(
+      resourceList.map(async (resource) => {
+        try {
+          const data = await resource.fn();
+          updatedResources[resource.name] = { status: "loaded", data };
+        } catch (error) {
+          logger.error(error);
+          updatedResources[resource.name] = { status: "errored", data: null };
+        }
+      }),
+    );
 
-  const resourcesToLoad = useMemo(
-    () => [
-      { name: GAME_DATA_QUERY_KEY, fn: gameDataApi.get },
-      { name: LOGIN_QUERY_KEY, fn: login },
-    ],
-    [gameDataApi.get, login],
-  );
+    // Batch update the state
+    setResources(updatedResources);
+  };
 
-  const initialResources =
-    useMultipleResources<[GameDataDto, UserDto]>(resourcesToLoad);
-
+  // Initialize resources. If mainApiBaseUrl changes, we'll need to reload all resources.
   useEffect(() => {
-    if (mainApiBaseUrl) setIsDataRequested(true);
+    setIsDataRequested(false);
+    const apiToLoad: ResourceToLoad<any>[] = [
+      { fn: login, name: USER_QUERY_KEY } as ResourceToLoad<UserDto>,
+      {
+        fn: gameDataApi.get,
+        name: GAME_DATA_QUERY_KEY,
+      } as ResourceToLoad<GameDataDto>,
+    ];
+    // Initialize all resources we know for sure we'll need.
+    initializeResources(apiToLoad);
+    setIsDataRequested(true);
   }, [mainApiBaseUrl]);
 
-  const allLoaded = useMemo(
-    () =>
-      isDataRequested &&
-      initialResources.every((resource) => resource.status === "loaded"),
-    [isDataRequested, initialResources],
-  );
-
-  const combinedResources = useMemo(() => {
-    return initialResources.reduce(
-      (acc, resource) => {
-        acc[resource.name] = resource as Resource<UserDto | GameDataDto>;
-        return acc;
-      },
-      { ...resources } as Record<
-        string,
-        Resource<unknown> & Resource<GameDataDto | UserDto>
-      >,
-    );
-  }, [initialResources, resources]);
-
-  useEffect(() => {
-    const loginData = combinedResources[LOGIN_QUERY_KEY];
-    loginData.promise?.catch((e) => {
-      errorContext.showErrorScreen({
-        message: e.message,
-        dismissable: false,
-      });
-    });
-  }, [combinedResources[LOGIN_QUERY_KEY]]);
-
-  useEffect(() => {
-    const resourceErrors = Object.values(combinedResources)
-      .filter(
-        (resource) =>
-          resource.status === "errored" && !(resource.name === LOGIN_QUERY_KEY),
-      )
-      .map((item) => ({ name: item.name, error: item.error }));
-
-    if (resourceErrors.length > 0) {
-      errorContext.showErrorScreen({
-        message:
-          "Something went wrong while fetching data. Please try again later.",
-        dismissable: false,
-      });
-      logger.error("Resource errors", resourceErrors);
-    }
-  }, [combinedResources]);
+  // if we haven't requested the data to load yet, our resource list might be empty.
+  const allLoaded =
+    isDataRequested &&
+    Object.values(resources).every((resource) => resource.status === "loaded");
 
   return (
     <ResourcesContext.Provider
-      value={{ updateResourceStatus, allLoaded, resources: combinedResources }}
+      value={{ updateResourceStatus, allLoaded, resources }}
     >
-      {allLoaded ? children : <LoadingScreen />}
+      {allLoaded ? children : <></>}
     </ResourcesContext.Provider>
   );
 };
 
 export const useResources = () =>
-  useContext(ResourcesContext) as ResourceContext;
+  useContext(ResourcesContext) as IResourcesContext;
