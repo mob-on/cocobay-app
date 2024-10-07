0858934e50036b30f79b284ba1e60068e39d8ab5
Ivan Vishnevskiy <io@forgetable.ru>
2024-10-07 15:14:40 +0400

    Added comments to potentially confusing code

--- packages/frontend/components/Stamina/Stamina.tsx
+++ packages/frontend/components/Stamina/Stamina.tsx
@@ -19,9 +19,13 @@ const Leveling: React.FC = () => {
   return (
     <div className="flex flex-col gap-half-inner self-end">
       <Battery className="w-auto h-[48px]" />
+      {/* This span is used to display the stamina value. It has absolute position, so that it doesn't affect the flexbox. */}
       <span className="absolute text-white bottom-0">
         {energy}/{maxEnergy}
       </span>
+      {/** We use this span to calculate the bounds, so that our flexbox doesn't change every time values change.
+       *   This happens because we don't use a monospace font, and every character has different width.
+       */}
       <span className="opacity-0">
         {maxEnergy}/{maxEnergy}
       </span>
